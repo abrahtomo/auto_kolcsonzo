42993dac62a753d7267961b1a632fcbb2b119919
unknown <tomoabraham92@gmail.com>
2024-11-11 18:36:38 +0100

    Vehicle list query is added to dao

--- src/main/java/hu/unideb/inf/repository/VehicleDAO.java
+++ src/main/java/hu/unideb/inf/repository/VehicleDAO.java
@@ -2,8 +2,11 @@ package hu.unideb.inf.repository;
 
 import hu.unideb.inf.model.Vehicle;
 
+import java.util.List;
+
 public interface VehicleDAO extends AutoCloseable {
     void insertVehicle(Vehicle vehicle);
+    List<Vehicle> getVehicles();
 
     @Override
     void close();
